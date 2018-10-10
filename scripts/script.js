4192d4028b0afe6db977eb90c2f453a1efd9d41f
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-10 11:59:46 +0300

    some fixed in js file

--- scripts/script.js
+++ scripts/script.js
@@ -2,37 +2,35 @@ let images = document.querySelectorAll('img');
 console.log(images);
 
 for (let i = images.length - 1; i >= 0; i--) {
-	images[i].addEventListener ('mouseover', function (e) {
-		changeImg(e.target);
-	}, false);
-	images[i].addEventListener ('mouseout', function (e) {
-		imgBack(e.target);
-	}, false);
-	images[i].addEventListener ('click', function (e) {
-		clickImg(e.target);
-	}, false);
+	images[i].addEventListener ('mouseover', changeImg, false);
+	images[i].addEventListener ('mouseout', imgBack, false);
 };
 
-function changeImg(target){
-	let className = target.className;
+function changeImg(e){
+	let target = e.target,
+		className = target.className;
 		if (className === 'river') {
 			target.attributes.src.nodeValue = "img/IMG_0282_600w.jpg";
 		} else if(className === 'fly'){
 			target.attributes.src.nodeValue = "img/IMG_0283_600w.jpg"
 		};
+		target.addEventListener ('click', clickImg, false);
 };
-function imgBack(target){
-	let className = target.className;
+function imgBack(e){
+	let target = e.target,
+		className = target.className;
 		if (className === 'river') {
 			target.attributes.src.nodeValue = "img/IMG_0471_600w.JPG";
 		} else if(className === 'fly'){
 			target.attributes.src.nodeValue = "img/IMG_0524_600w.JPG"
 		};
 };
-function clickImg(target){
-	let className = target.className;
+function clickImg(e){
+	console.log(e);
+	let target = e.target,
+		className = target.className;
 		if (className === 'river') {
-			target.attributes.src.nodeValue = "img/IMG_9387_600w.jpg";
+			target.attributes.src.nodeValue = "img/IMG_9387_600w.JPG";
 		} else if(className === 'fly'){
 			target.attributes.src.nodeValue = "img/IMG_9464_600w.JPG"
 		};
