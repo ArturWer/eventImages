14621e1867031ed0eaa149eef415207693ed67f4
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-10 12:17:35 +0300

    some fixed in js file

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,22 @@ console.log(images);
 for (let i = images.length - 1; i >= 0; i--) {
 	images[i].addEventListener ('mouseover', changeImg, false);
 	images[i].addEventListener ('mouseout', imgBack, false);
+	images[i].addEventListener ('click', clickImg, false);
 };
 
 function changeImg(e){
+	console.log(`Make function changeImg`);
 	let target = e.target,
 		className = target.className;
+		console.log(target);
 		if (className === 'river') {
 			target.attributes.src.nodeValue = "img/IMG_0282_600w.jpg";
 		} else if(className === 'fly'){
 			target.attributes.src.nodeValue = "img/IMG_0283_600w.jpg"
 		};
-		target.addEventListener ('click', clickImg, false);
 };
 function imgBack(e){
+	console.log(`Make function imgBack`);
 	let target = e.target,
 		className = target.className;
 		if (className === 'river') {
@@ -26,9 +29,10 @@ function imgBack(e){
 		};
 };
 function clickImg(e){
-	console.log(e);
+	console.log(`Make function clickImg`);
 	let target = e.target,
 		className = target.className;
+	console.log(target);
 		if (className === 'river') {
 			target.attributes.src.nodeValue = "img/IMG_9387_600w.JPG";
 		} else if(className === 'fly'){
