032348f82c2faeb88d52ee7922d9095e99df480d
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-11 05:47:09 +0300

    changed eventListener. Add img:hover click listener

--- scripts/script.js
+++ scripts/script.js
@@ -4,22 +4,20 @@ console.log(images);
 for (let i = images.length - 1; i >= 0; i--) {
 	images[i].addEventListener ('mouseover', changeImg, false);
 	images[i].addEventListener ('mouseout', imgBack, false);
-	images[i].addEventListener ('click', clickImg, false);
 };
 
 function changeImg(e){
-	console.log(`Make function changeImg`);
 	let target = e.target,
 		className = target.className;
-		console.log(target);
 		if (className === 'river') {
 			target.attributes.src.nodeValue = "img/IMG_0282_600w.jpg";
 		} else if(className === 'fly'){
 			target.attributes.src.nodeValue = "img/IMG_0283_600w.jpg"
 		};
+	let changedImg = document.querySelector('img:hover');
+	changedImg.addEventListener ('click', clickImg, false);
 };
 function imgBack(e){
-	console.log(`Make function imgBack`);
 	let target = e.target,
 		className = target.className;
 		if (className === 'river') {
@@ -29,10 +27,8 @@ function imgBack(e){
 		};
 };
 function clickImg(e){
-	console.log(`Make function clickImg`);
 	let target = e.target,
 		className = target.className;
-	console.log(target);
 		if (className === 'river') {
 			target.attributes.src.nodeValue = "img/IMG_9387_600w.JPG";
 		} else if(className === 'fly'){
